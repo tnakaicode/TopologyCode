e9ce5a4c957763b21219eaaacd8d72970dab62cf
tnakaicode <tnakaicode@gmail.com>
2022-12-28 20:15:19 +0900

    first commit

---
+++ plot_topologicpy.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import platform
+import time
+import argparse
+from linecache import getline, clearcache, updatecache
+
+sys.path.append(os.path.join("../../"))
+import topologicpy
+import topologic
+
+from topologicpy.Aperture import Aperture
+from topologicpy.Cell import Cell
+from topologicpy.CellComplex import CellComplex
+from topologicpy.Cluster import Cluster
+from topologicpy.Color import Color
+from topologicpy.Context import Context
+from topologicpy.DGL import DGL
+from topologicpy.Dictionary import Dictionary
+from topologicpy.Edge import Edge
+from topologicpy.EnergyModel import EnergyModel
+from topologicpy.Face import Face
+from topologicpy.Graph import Graph
+from topologicpy.Graph_Export import Graph_Export
+from topologicpy.Grid import Grid
+from topologicpy.Helper import Helper
+from topologicpy.Honeybee import Honeybee
+from topologicpy.Matrix import Matrix
+from topologicpy.Neo4jGraph import Neo4jGraph
+from topologicpy.Plotly import Plotly
+from topologicpy.Process import Process
+from topologicpy.Shell import Shell
+from topologicpy.Speckle import Speckle
+from topologicpy.SQL import SQL
+from topologicpy.Topology import Topology
+from topologicpy.UnitTest import UnitTest
+from topologicpy.Vector import Vector
+from topologicpy.Vertex import Vertex
+from topologicpy.Wire import Wire
+
+# ./img/topologicpy.jpg
+# conda install pytorch torchvision torchaudio cpuonly -c pytorch
+# conda install -c dglteam dgl
+# conda install -c plotly plotly
+# pip install openstudio
+
+
+# from base import plot2d, plot3d
+# from base_occ import dispocc
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", dest="dir", default="./")
+    parser.add_argument("--pxyz", dest="pxyz",
+                        default=[0.0, 0.0, 0.0], type=float, nargs=3)
+    opt = parser.parse_args()
+    print(opt, argvs)
+    
+    building = CellComplex.Box(uSides=4, vSides=2, wSides=3)
+    atrium = Cell.Box(width=0.5, length=0.5)
+    building = Topology.Boolean(building, atrium, "impose")
+    buildingData = Plotly.DataByTopology(building)
+    
+    plotlyData = buildingData
+    plotlyFigure = Plotly.FigureByData(plotlyData)
+    Plotly.Show(plotlyFigure)
+
