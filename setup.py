24dfb5f05f5532ca696d19c4963f8ea739ec7e76
tnakaicode <tnakaicode@gmail.com>
2023-03-03 09:36:52 +0900

    setup tool

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(name='topologicpy',
+      version='0.2.2',
+      description='Topologic',
+      license='GPL',
+      url="https://github.com/wassimj/topologicpy.git",
+      author="wassimj",
+      author_email="jabiw@cardiff.ac.uk",
+      packages=['topologicpy'],
+      package_dir={'topologicpy': 'topologicpy'},
+      package_data={},
+      zip_safe=False
+      )
