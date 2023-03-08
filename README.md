97b217f9ac7cc7958bb4d80471f643523c3a2a86
tnakaicode <tnakaicode@gmail.com>
2023-03-08 18:24:19 +0900

    setup

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ```bash
 conda env create -f py39_math.yaml
 ```
+
+<https://topologic.app/topologicpy_doc/topologic_pdoc/>
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='topologicpy',
-      version='0.2.2',
+      version='0.2.5',
       description='Topologic',
       license='GPL',
       url="https://github.com/wassimj/topologicpy.git",
